b0de61813237fa0ec20880db8249c14047069531
Sam Freeman <samcfreeman888@gmail.com>
2022-07-18 15:44:59 -0500

    finished with required part 1 goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -53,7 +53,7 @@ console.log(`Ro's Pets:`, friendsPets);
 // 11 - Add two pets to your `pets` variable
 
 pets += 2;
-console.log(pets);
+console.log('pets now:', pets);
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
@@ -73,11 +73,26 @@ else {
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
 
+if(luckyNumber === 2 && adventurous === true){
+    console.log('Roll the dice!');
+}
+else {
+    console.log('No dice!');
+}
+
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
+if(pets < allowedPets){
+    console.log('I can have more pets!');
+} else if(pets === allowedPets){
+    console.log('I have enough pets');
+} else {
+    console.log('Oh no, I have too many pets');
+}
 
 // STRETCH GOALS:
 
