d2acf39aecab3d1c7291656855e388780561888c
Sam Freeman <samcfreeman888@gmail.com>
2022-07-18 16:21:56 -0500

    completed through last question

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -31,7 +31,7 @@ console.log(`My name is ${fullName} and I think ${luckyNumber} is a winner!`)
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 
-let adventurous = true;
+let adventurous = false;
 console.log('adventurous:', adventurous);
 
 
@@ -102,6 +102,35 @@ if(pets < allowedPets){
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
 
+
+let mostPets;
+
+if(pets > friendsPets){
+    mostPets = pets;
+} else if (pets < friendsPets) {
+    mostPets = friendsPets;
+} else {
+    mostPets = pets;
+    console.log('You and Ro have the same number of pets');
+}    
+
+console.log(mostPets); 
+
+//If we don't care about what happens when they are equal it could be less redundant and be:
+
+
+/*
+let mostPets;
+
+if (pets > friendsPets){
+    mostPets = pets;
+} else {
+    mostPets = friendsPets;
+}
+console.log(mostPets);
+*/
+
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
@@ -109,6 +138,24 @@ if(pets < allowedPets){
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
+switch(luckyNumber){
+    case 1:
+        console.log('First is the worst');
+        break;
+    case 2:
+        console.log('Second is the best');
+        break;
+    case 3:
+        console.log('Third is the one with the polka dress');
+        break;
+    default:
+        console.log('Luck is what happens when preparation meets opportunity');
+}
+
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+
+adventurous = true ? console.log('Adventures are great!') : console.log('How about we stay home?');
+
+
 
 
