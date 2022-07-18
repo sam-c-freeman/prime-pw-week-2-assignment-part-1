9cc6988a13ee073af7f1a16eaae7b11d823ec481
Sam Freeman <samcfreeman888@gmail.com>
2022-07-18 15:21:48 -0500

    started working on js script answers

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,22 +1,44 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 
+let firstName = 'Sam';
+console.log(firstName);
+
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+
+let lastName = 'Freeman';
+console.log(lastName);
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
 // 4 - Console log the value of `fullName`
 
+console.log(fullName);
+
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+
+let luckyNumber = 8;
+console.log(luckyNumber);
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
+console.log(`My name is ${fullName} and I think ${luckyNumber} is a winner!`)
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 
+let adventurous = true;
+console.log('adventurous:', adventurous);
+
+
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
+
+let food = 'chocolate covered strawberries';
+console.log('favorite food:', food);
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 
